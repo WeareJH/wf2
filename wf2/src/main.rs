6d26be67cef52b5faae5115fa2e8263f87eaaab3
Shane Osbourne <shane.osbourne8@gmail.com>
2019-05-25 15:22:24 +0100

    adding clap

--- wf2/src/main.rs
+++ wf2/src/main.rs
@@ -1,31 +1,54 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 use futures::{future::lazy, future::Future};
 use std::path::PathBuf;
 use wf2_core::recipes::Recipe;
 use wf2_core::{context::Context, WF2};
+use std::env::current_dir;
 
 fn main() {
-    let ctx = Context::new(current_working_dir());
-    let recipe = Recipe::m2_php_7_2();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
-    tokio::run(lazy(move || {
-        let (tasks, fut) = WF2::exec(ctx, recipe);
-        let tasks_len = tasks.len();
-        fut.map(|_| {
-            println!("All done");
-            ()
-        })
-        .map_err(move |(task, task_error)| {
-            eprintln!("{}", task_error);
-            eprintln!("\nThis error occurred in the following task:\n");
-            eprintln!("    [Task] {}", task);
-            eprintln!(
-                "\nSummary: {} complete, 1 errored, {} didn't start",
-                task_error.index,
-                tasks_len - task_error.index - 1
-            );
-            ()
-        })
-    }));
+    let cwd = matches.value_of("cwd")
+        .map(PathBuf::from)
+        .unwrap_or(current_dir().unwrap()); // if getting the CWD panics on unwrap, there's zero hope anyway
+
+    let ctx = Context::new(cwd);
+
+    match matches.subcommand() {
+        ("up", Some(matches)) => {
+            println!("hello there up");
+        }
+        _ => {
+            println!("hello");
+        }
+    }
+
+
+//    let recipe = Recipe::m2_php_7_2();
+//
+//    tokio::run(lazy(move || {
+//        let (tasks, fut) = WF2::exec(ctx, recipe);
+//        let tasks_len = tasks.len();
+//        fut.map(|_| {
+//            println!("All done");
+//            ()
+//        })
+//        .map_err(move |(task, task_error)| {
+//            eprintln!("{}", task_error);
+//            eprintln!("\nThis error occurred in the following task:\n");
+//            eprintln!("    [Task] {}", task);
+//            eprintln!(
+//                "\nSummary: {} complete, 1 errored, {} didn't start",
+//                task_error.index,
+//                tasks_len - task_error.index - 1
+//            );
+//            ()
+//        })
+//    }));
 }
 
 pub fn current_working_dir() -> PathBuf {
