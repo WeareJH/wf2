d9f09179efdcd18162206cf5526a810264aa1bf6
Diego Cabrejas <diego@wearejh.com>
2019-06-21 09:28:23 +0100

    Add contrib recipe test

--- wf2/src/tests/mod.rs
+++ wf2/src/tests/mod.rs
@@ -90,6 +90,25 @@ mod tests {
     }
 
     #[test]
+    fn test_m2_contrib_recipe() {
+        let args = vec!["prog", "--config", "../fixtures/config_contrib.yaml", "up"];
+        let cli_output = CLIOutput::from_input(CLIInput {
+            args: args.into_iter().map(String::from).collect(),
+            ..CLIInput::default()
+        });
+        let unison_bytes = include_bytes!("../../../wf2_core/src/recipes/m2_contrib/templates/sync.prf");
+
+        let tasks = cli_output.unwrap().tasks.unwrap();
+        let unison_task = tasks.get(5).unwrap();
+        match unison_task {
+            Task::File{ kind: FileOp::Write { content }, ..} => {
+                assert_eq!(unison_bytes.to_vec(), *content);
+            },
+            _ => unreachable!()
+        }
+    }
+
+    #[test]
     fn test_php_version_in_config() {
         let args = vec!["prog", "--config", "../fixtures/config_php_71.yaml", "up"];
         let cli_output = CLIOutput::from_input(CLIInput {
