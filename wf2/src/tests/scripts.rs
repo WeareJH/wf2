8e43c7024ce21ab5d2cc54fb2ba4498393451fa1
Shane Osbourne <shane.osbourne8@gmail.com>
2019-10-01 17:36:12 +0100

    added high-level tests for scripts

--- wf2/src/tests/scripts.rs
+++ wf2/src/tests/scripts.rs
@@ -3,18 +3,54 @@ mod tests {
     use crate::cli_input::CLIInput;
     use crate::cli_output::CLIOutput;
     use crate::tests::commands;
+    use std::path::PathBuf;
 
     #[test]
-    fn test_scripts() {
-        use std::path::PathBuf;
+    fn test_scripts_multiple() {
         let args = vec!["prog", "--config", "../fixtures/config_01.yaml", "bundle"];
+        let cmds = _test(args);
+        assert_eq!(cmds.len(), 5);
+    }
+
+    #[test]
+    fn test_dc_run_script() {
+        let args = vec!["prog", "--config", "../fixtures/config_01.yaml", "dc_run"];
+        let cmds = _test(args);
+        let expected = r#"docker-compose -f /users/acme/.wf2_m2_acme/docker-compose.yml run --user root node echo hello"#;
+        assert_eq!(expected, cmds.get(0).expect("test"));
+    }
+
+    #[test]
+    fn test_dc_exec_script() {
+        let args = vec!["prog", "--config", "../fixtures/config_01.yaml", "dc_exec"];
+        let cmds = _test(args);
+        let expected = r#"docker-compose -f /users/acme/.wf2_m2_acme/docker-compose.yml exec --user root node echo hello"#;
+        assert_eq!(expected, cmds.get(0).expect("test"));
+    }
+
+    #[test]
+    fn test_dc_pass_through() {
+        let args = vec!["prog", "--config", "../fixtures/config_01.yaml", "dc_pass_thru"];
+        let cmds = _test(args);
+        let expected = r#"docker-compose -f /users/acme/.wf2_m2_acme/docker-compose.yml logs unison"#;
+        assert_eq!(expected, cmds.get(0).expect("test"));
+    }
+
+    #[test]
+    fn test_sh() {
+        let args = vec!["prog", "--config", "../fixtures/config_01.yaml", "shell_script"];
+        let cmds = _test(args);
+        let expected = r#"echo hello world"#;
+        assert_eq!(expected, cmds.get(0).expect("test"));
+    }
+
+    fn _test(args: Vec<&str>) -> Vec<String> {
         let cli_output = CLIOutput::from_input(CLIInput {
             args: args.into_iter().map(String::from).collect(),
             cwd: PathBuf::from("/users/acme"),
             ..CLIInput::default()
         })
-        .unwrap();
-        let cmds = commands(cli_output.tasks.expect("test"));
-        //        assert_eq!(cmds.len(), 3);
+            .unwrap();
+        commands(cli_output.tasks.expect("test"))
     }
 }
