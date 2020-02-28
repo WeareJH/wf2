c2f2e14ff105848d94038bf23a25aa59a2019e33
Aydin Hassan <aydin@hotmail.co.uk>
2020-02-28 18:41:51 +0100

    Only stop and rm containers, if there are some running

--- wf2_core/src/tasks/docker_clean.rs
+++ wf2_core/src/tasks/docker_clean.rs
@@ -2,7 +2,7 @@ use crate::task::Task;
 
 pub fn docker_clean() -> Vec<Task> {
     vec![
-        Task::simple_command("docker stop $(docker ps -aq)"),
-        Task::simple_command("docker rm $(docker ps -aq)"),
+        Task::simple_command("if [[ $(docker ps -aq) ]]; then docker stop $(docker ps -aq); fi"),
+        Task::simple_command("if [[ $(docker ps -aq) ]]; then docker rm $(docker ps -aq); fi"),
     ]
 }
